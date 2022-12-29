7e00a87bbffccee7d46c974e7bfce126daf7de0d
Alexander Strakh <cromlehg@gmail.com>
2022-12-29 20:02:16 +0300

    Rist project

---
+++ src/main/java/com/blockwit/learn/ruslan/App.java
@@ -0,0 +1,13 @@
+package com.blockwit.learn.ruslan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/blockwit/learn/ruslan/AppTest.java
@@ -0,0 +1,20 @@
+package com.blockwit.learn.ruslan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
