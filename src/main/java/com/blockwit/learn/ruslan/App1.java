a48c2d374a25811a8746d2efe99c9ace4dda21ec
ruslan1811 <ruslangirfanov1@gmail.com>
2023-01-01 19:12:11 +0300

    corrected tab

--- src/main/java/com/blockwit/learn/ruslan/App1.java
+++ src/main/java/com/blockwit/learn/ruslan/App1.java
@@ -4,8 +4,8 @@ package com.blockwit.learn.ruslan;
 
 public class App1 {
 	public static void main(String[] args) {
-	
-		int[] monthSalaries = { 100,109,108,114,101,100,120,200,103,112,107,150};		
+
+	int[] monthSalaries = { 100,109,108,114,101,100,120,200,103,112,107,150};		
 	double sum = 0;
 	for (int i = 0; i < monthSalaries.length; i++) {
 		sum += monthSalaries[i];
@@ -13,5 +13,6 @@ public class App1 {
 	
 	double salaryVacation = sum / monthSalaries.length;
 	System.out.println(salaryVacation);
+
 }
 }
