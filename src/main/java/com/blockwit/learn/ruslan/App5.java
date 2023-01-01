0ec802299b78aa86f97430d56f6846d579ad9a3f
ruslan1811 <ruslangirfanov1@gmail.com>
2023-01-01 23:11:56 +0300

    task 5

---
+++ src/main/java/com/blockwit/learn/ruslan/App5.java
@@ -0,0 +1,14 @@
+package com.blockwit.learn.ruslan;
+
+	
+
+public class App5 {
+	public static void main(String[] args) {
+		String[] names = {"Alice", "Bob", "John", "Eugeny", "Alex", "Mira", "Serg", "Ilon"};
+		for (int i = 0; i < names.length - 1; i = i + 2) {
+			System.out.println(names[i]);
+		}	
+
+
+}
+}
