6e08c1cf7b27fda074662527a6b92929bef2054b
ruslan1811 <ruslangirfanov1@gmail.com>
2023-01-01 20:26:21 +0300

    task 3 and 4

---
+++ src/main/java/com/blockwit/learn/ruslan/App3.java
@@ -0,0 +1,15 @@
+package com.blockwit.learn.ruslan;
+
+	
+
+public class App3 {
+	public static void main(String[] args) {
+
+	String[] names = {"Alice", "Bob", "John" };
+	
+	for (int i = 0; i < 3; i ++ ) {
+	System.out.println(names[i]);
+	}
+
+}
+}
---
+++ src/main/java/com/blockwit/learn/ruslan/App4.java
@@ -0,0 +1,14 @@
+package com.blockwit.learn.ruslan;
+
+	
+
+public class App4 {
+	public static void main(String[] args) {
+
+	String[] names = {"Alice", "Bob", "John" };
+	for (int i = names.length-1; i >= 0; i-- ) {
+	System.out.println(names[i]);
+	}
+
+}
+}
