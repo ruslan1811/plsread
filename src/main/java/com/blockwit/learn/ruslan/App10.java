d616aa59019b0f4e9ab77d344e84d143cc984e9b
ruslan1811 <ruslangirfanov1@gmail.com>
2023-01-02 16:38:44 +0300

    task 11

---
+++ src/main/java/com/blockwit/learn/ruslan/App10.java
@@ -0,0 +1,13 @@
+package com.blockwit.learn.ruslan;
+
+	
+
+public class App10 {
+	public static void main(String[] args) {
+		for (int i = -100; i <= -20; i ++) {
+			System.out.println(i);
+		}	
+
+
+}
+}
---
+++ src/main/java/com/blockwit/learn/ruslan/App11.java
@@ -0,0 +1,13 @@
+package com.blockwit.learn.ruslan;
+
+	
+
+public class App11 {
+	public static void main(String[] args) {
+		for (int i = -20; i >= -100; i --) {
+			System.out.println(i);
+		}	
+
+
+}
+}
