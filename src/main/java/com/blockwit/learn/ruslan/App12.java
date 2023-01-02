e03d1ff99808a7691c3e2c887c79339c95288791
ruslan1811 <ruslangirfanov1@gmail.com>
2023-01-02 21:27:46 +0300

    task 12 with for

--- src/main/java/com/blockwit/learn/ruslan/App12.java
+++ src/main/java/com/blockwit/learn/ruslan/App12.java
@@ -5,13 +5,12 @@ package com.blockwit.learn.ruslan;
 public class App12 {
 	public static void main(String[] args) {
 		
-	int i = 0;
-	int b = 10;
-	do { for (int a = 50; a <=100; a ++) {
+	for (int i = 0; i <=10;) {
+       	 for (int a = 50; a <=100; a ++) {
 		System.out.println(a);
 		}
 		i++;	
-	 } while (i < b);
+	 } 
 
 
 }
