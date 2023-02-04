da5f0f703802b43ab5339346064739103c4a3dd6
ruslan1811 <ruslangirfanov1@gmail.com>
2023-02-05 00:06:27 +0300

    task 52

---
+++ src/main/java/com/blockwit/learn/ruslan/App52.java
@@ -0,0 +1,32 @@
+package com.blockwit.learn.ruslan;
+
+
+class Book {
+
+//52. Написать класс книги. У книги должно быть название, автор и год выпуска
+
+    String name;
+    String author;
+    int yearOfRelease;
+
+
+
+}
+
+
+public class App52 {
+
+    public static void main (String args[]) {
+
+    Book book = new Book();
+    book.name = "Богатый папа, Бедный папа";
+    book.author = "Роберт Кийосаки";
+    book.yearOfRelease = 2011;
+
+
+
+
+
+    }
+
+}
