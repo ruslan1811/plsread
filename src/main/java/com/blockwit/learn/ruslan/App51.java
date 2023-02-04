6e86e5fb11401a8fdacf27c353f41908459ccc45
ruslan1811 <ruslangirfanov1@gmail.com>
2023-02-04 23:59:13 +0300

    task 51

---
+++ src/main/java/com/blockwit/learn/ruslan/App51.java
@@ -0,0 +1,34 @@
+package com.blockwit.learn.ruslan;
+
+
+class Planet {
+
+
+    String name;
+    String diameter;
+    String distance;
+    int numberFromSun;
+
+}
+
+
+
+public class App51 {
+
+    public static void main(String args[]) {
+
+    Planet planet = new Planet();
+
+    planet.name = "Jupiter";
+
+    planet.diameter = "139 820";
+
+    planet.distance = "778 570 000";
+
+    planet.numberFromSun = 5;
+
+
+
+    }
+
+}
