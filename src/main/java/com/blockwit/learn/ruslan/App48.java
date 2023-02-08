00a1b25d19c113dfc12b8663a8ecb10cd786aed6
ruslan1811 <ruslangirfanov1@gmail.com>
2023-02-08 09:03:13 +0300

    task 48 corrected

--- src/main/java/com/blockwit/learn/ruslan/App48.java
+++ src/main/java/com/blockwit/learn/ruslan/App48.java
@@ -3,30 +3,46 @@ import java.util.Arrays;
 
 public class App48 {
 
-
     public static void main (String args[]) {
-
-
 
         int[] numbers = {12,7,89,-4,8,9};
 
-
-        for (int i : numbers) {
+        for (int i= 0; i < numbers.length -1; i++) {
             System.out.println(i);
 
+        }
 
+        int lastNumber = numbers[numbers.length -1];
+
+        for (int j = numbers.length -1; j >=0; j--) {
+
+            if (numbers[j] < lastNumber) lastNumber = numbers[j];
 
         }
 
-        Arrays.sort(numbers);
+        boolean swapped = true;
+        int k = 0;
+        int temporary;
 
-        for (int values : numbers) {
-            System.out.print(values + ", ");
+        while (swapped) {
+            swapped = false;
+            k++;
+            for( int i=0; i< numbers.length - k; i++) {
+                if (numbers[i] > numbers[i+1]) {
+                    temporary = numbers[i];
+                    numbers[i] = numbers[i+1];
+                    numbers[i+1] = temporary;
+                    swapped = true;
 
+                }
+
+            }
         }
 
+        for (int i =0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+ "; ");
 
-
+        }
 
     }
 
