fd59c6e5cf1e6c7d6663a7a1359aa002929cfea3
ruslan1811 <ruslangirfanov1@gmail.com>
2023-02-08 09:37:32 +0300

    task 53

--- src/main/java/com/blockwit/learn/ruslan/task53/Pet.java
+++ src/main/java/com/blockwit/learn/ruslan/task53/Pet.java
@@ -1,16 +1,11 @@
 package com.blockwit.learn.ruslan.task53;
 
 public class Pet {
-
         int yearOfBirth;
         String color;
         String name;
-
         int calculateAge () {
             int age;
             return 2023 - yearOfBirth;
         }
-
-
-
 }
