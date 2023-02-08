972ed088296037d12bbad5ec4ce98870bd4f4a28
ruslan1811 <ruslangirfanov1@gmail.com>
2023-02-08 09:11:50 +0300

    task 49

--- src/main/java/com/blockwit/learn/ruslan/App49.java
+++ src/main/java/com/blockwit/learn/ruslan/App49.java
@@ -1,29 +1,19 @@
 package com.blockwit.learn.ruslan;
 
 public class App49 {
-
     public static void main (String args[]) {
-
 
         int[] firstArray = {7,4,2,8,6,7};
         int[] secondArray = {6,2,9};
 
-        for (int i : firstArray) {
-            System.out.println(i);
+        for (int i =0; i < firstArray.length; i++) {
+            System.out.println(firstArray[i]);
         }
-        for (int i : secondArray) {
-            System.out.println(i);
+        for (int i =0;  i < secondArray.length; i++) {
+            System.out.println(secondArray[i]);
         }
 
         int[] resultArray = new int[firstArray.length + secondArray.length];
-
-//       for (int i = 0; i < secondArray.length ; i++){
-//            resultArray[i * 2] = firstArray[i];
-//            resultArray[i * 2 + 1] = secondArray[i];
-//        System.out.println("Вывод" + resultArray[i]);
-//
-//       }
-
 
         int maxLen = Math.max(firstArray.length, secondArray.length), j =0;
         System.out.println(maxLen);
@@ -35,58 +25,18 @@ public class App49 {
 
         }
 
-
         int lastLen = resultArray.length - firstArray.length;
-
-//        System.out.println(lastLen);
-//        System.out.println(maxLen);
-
 
         int k = lastLen;
         for (int i = maxLen; i < resultArray.length; i++) {
-
-//            if (i > maxLen) resultArray[k++] = firstArray[i++];
                 System.out.println("Вывод - " + resultArray[i]);
 
         }
 
-
-//        for ( j = 6;  j < firstArray.length; j++ ) {
-//
-//        if ( resultArray[j] == ( j  % 2)) {
-//             resultArray[j++] = firstArray[j++];
-//            }
-//
-//
-//            System.out.println("txt" + resultArray[j]);
-//
-//        }
-//        for (int k =0;  k < secondArray.length; k++ ) {
-//
-//            if ( k % 2 == 0) {
-//                resultArray[k + 1] = secondArray[k];
-//            }
-//
-//            System.out.println("txt" + resultArray[k]);
-//
-//        }
-
-//        for (int i =0; i < 7; i++) {
-//
-//            System.out.println("Вывод" + resultArray[i]);
-//
-//        }
-//
-
-
-
-
-
-
-
+        for (int i =0; i < firstArray.length; i++) {
+            System.out.println(resultArray[i]);
+        }
+        
     }
-
-
-
 
 }
