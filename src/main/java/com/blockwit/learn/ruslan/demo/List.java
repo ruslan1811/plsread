b6740a7b4ab3cf047e05f378c0c22116b43745ee
ruslan1811 <ruslangirfanov1@gmail.com>
2023-02-27 06:59:34 +0300

    performace task with changes

--- src/main/java/com/blockwit/learn/ruslan/demo/List.java
+++ src/main/java/com/blockwit/learn/ruslan/demo/List.java
@@ -2,22 +2,24 @@ package com.blockwit.learn.ruslan.demo;
 
 public class List {
     static int[] list;
-    static int index = 0;
-    long startTime = System.currentTimeMillis();
 
-    public  void  addItem(int Number) {
-         list = new int[100000];
-         for (int i=0; i < list.length; i++) {
-             list[index] = Number;
-             index++;
-     }
-    }
+   private int index = 0;
 
-    public long calculateTime() {
-        long finishTime = System.currentTimeMillis();
-        long result = finishTime - startTime;
-        System.out.println(result);
-        return result;
-    }
+//    long startTime = System.currentTimeMillis();
+//
+//    public  void  addItem(int Number) {
+//         list = new int[100000];
+//         for (int i=0; i < list.length; i++) {
+//             list[index] = Number;
+//             index++;
+//     }
+//    }
+//
+//    public long calculateTime() {
+//        long finishTime = System.currentTimeMillis();
+//        long result = finishTime - startTime;
+//        System.out.println(result);
+//        return result;
+//    }
 
 }
--- src/main/java/com/blockwit/learn/ruslan/demo/TestList.java
+++ src/main/java/com/blockwit/learn/ruslan/demo/TestList.java
@@ -3,17 +3,23 @@ package com.blockwit.learn.ruslan.demo;
 import java.util.Date;
 
 public class TestList {
- public static void main (String args[]) {
-     List list = new List();
-     testCells();
-     list.calculateTime();
-     }
+    public static void main(String args[]) {
+        List list = new List();
+        testPerformance();
+    }
 
-    public static void testCells() {
+    public  static long testPerformance() {
+        long startTime = System.currentTimeMillis();
         List varList = new List();
-        varList.addItem(4000000);
-         for (int i=0; i< varList.list.length; i++) {
-             System.out.println(varList.list[i]);
-         }
-     }
+        varList.list = new int[100000];
+        for (int i = 0; i < varList.list.length; i++) {
+            System.out.println(varList.list[i]);
+        }
+        long finishTime = System.currentTimeMillis();
+        long result = finishTime - startTime;
+       System.out.println(result);
+       return result;
+    }
+
+
 }
