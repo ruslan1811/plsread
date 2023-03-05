55533aac80a86e9cc44c900fa77becea815fb7c4
ruslan1811 <ruslangirfanov1@gmail.com>
2023-03-05 18:35:50 +0300

    task count

--- src/main/java/com/blockwit/learn/ruslan/demo/List.java
+++ src/main/java/com/blockwit/learn/ruslan/demo/List.java
@@ -2,14 +2,12 @@ package com.blockwit.learn.ruslan.demo;
 import java.util.Arrays;
 
 public class List {
-    public static String[] list;
+    private static String[] list = new String[100000];;
     private int index = 0;
-
 
  public void add (String item) {
      if (index == list.length) {
          list = Arrays.copyOf(list, list.length + 1);
-
      }
      list[index] = item;
      index++;
@@ -17,7 +15,6 @@ public class List {
 
  public String get (int idOfItem) {
      return list[idOfItem];
+
  }
-
-
 }
--- src/main/java/com/blockwit/learn/ruslan/demo/TestList.java
+++ src/main/java/com/blockwit/learn/ruslan/demo/TestList.java
@@ -5,21 +5,12 @@ import java.util.Date;
 public class TestList {
     public static void main(String args[]) {
         List list = new List();
-        testPerformance();
-    }
-    public  static long testPerformance() {
         long startTime = System.currentTimeMillis();
-        List copyList = new List();
-        copyList.list = new String[100000];
-        for (int i = 0; i < copyList.list.length; i++) {
-            System.out.println(copyList.list[i]);
+    for (int i = 0; i < 100000; i++) {
+            list.add("Text");
         }
-        long finishTime = System.currentTimeMillis();
-        long result = finishTime - startTime;
-       System.out.println(result);
-       return result;
+        long endTime = System.currentTimeMillis();
+        long result = endTime - startTime;
+        System.out.println(result);
     }
-
-
-
 }
